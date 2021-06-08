6964cd79181b4e1535b520ac20a60e2b88585fd5
SolJCam <scameron110@gmail.com>
2021-06-07 22:06:15 -0400

    first use of a react hook: useState, to dynamically update semantic ui classes

--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -1,16 +1,25 @@
-import React from 'react';
+import React, {useState} from 'react';  // useState is the first use of a React Hook thus far in this project
 
 const Accordion = ({items}) => {
+    // second value in a desctructured useState hook is allways a setter function regardless of what you name it
+    const [ activeIndex, setActiveIndex ] = useState(null); // for rurther explanation of these variable names ref vid 144
+
+    const onTitleClick = index => {
+        setActiveIndex(index);
+    }; 
+
     const renderedItems = items.map((item, index) => {
+        const active = index == activeIndex ? "active" : "";
+
         return(
             // read up on what React.Fragment is 
             // but here it's purpose is to prevent an extra border when using a div as parent element
             <React.Fragment key={ item.title }>
-                <div className="title active" onClick={()=>console.log('Title clicked',index)}>
+                <div className= {`title ${active}`} onClick={()=>onTitleClick(index)}>
                     <i className="dropdown icon"></i>
                     { item.title }
                 </div>
-                <div className="content active">
+                <div className={`content ${active}`}>
                     <p>{ item.content }</p>
                 </div>
             </React.Fragment>
@@ -18,7 +27,9 @@ const Accordion = ({items}) => {
     })
 
     return(
-        <div className="ui styled accordion">{ renderedItems }</div>
+        <div className="ui styled accordion">
+            { renderedItems }
+        </div>
     );
 };
 
