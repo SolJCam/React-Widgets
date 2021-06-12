4d6d1d644dc7eb53b76fdbfd86b572f4f2972af0
SolJCam <scameron110@gmail.com>
2021-06-12 13:37:43 -0400

    Successfully applied useEffect and axios to make wikipedia API calls. Data rendered in browser

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,12 +1,51 @@
 import React, {useState, useEffect} from 'react';
+import axios from 'axios';
 
 const Search = () => {
     const [term, setTerm] = useState('');
+    const [results, setResults] = useState([]);
 
-    //  first arguement is always a callback function, second either nothing OR an array
+    // useEffect runs after every initial render
+    //  first arguement is always a callback function
+    // second argument is either an empty array, no argument (runs again after every RE-RENDER) OR an array containing data (runs if data has changed since last reder).
     useEffect(()=>{
-        console.log('adfaf');
-    });
+        const apiRequest = async () => {
+            const {data} = await axios.get('https://en.wikipedia.org/w/api.php', {
+                params: {
+                    action: 'query',
+                    list: 'search',
+                    origin: '*',
+                    format: 'json',
+                    srsearch: term,
+                }
+            });
+
+            // console.log(data.query.search);
+            setResults(data.query.search);
+        }; 
+        
+        if(term){
+            apiRequest();
+        };
+        
+    }, [term]);
+
+    const renderedResults = results.map((result) => {
+        return(
+            <div key={result.pageid} className='item'>
+                <div className='right floated content'>
+                    <a className='ui button'>Go</a>
+                </div>
+                <div className='content'>
+                    <div className='header'>
+                        {result.title}
+                    </div>
+                    {result.snippet}
+                </div>
+            </div>
+                
+        )
+    })
 
     return(
         <div>
@@ -20,6 +59,7 @@ const Search = () => {
                     />
                 </div>
             </div>
+            <div className='ui celled list'>{renderedResults}</div>
         </div>
     );
 };
