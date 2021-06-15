21a435a65ebc936cec722d1ab4ca61401bb36141
SolJCam <scameron110@gmail.com>
2021-06-14 22:13:17 -0400

    introduced useRef hook to avoid common error that occus when trying to close dropdown by clicking outside the element

--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -1,7 +1,20 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect, useRef} from 'react';
 
 const Dropdown = ({items, selected, onSelectedChange}) => {
     const [open, setOpen] = useState(false);
+    const ref = useRef();      // this hook allows you to return whatever dom element you attach it to 
+
+    useEffect(()=>{
+        document.addEventListener('click', (event)=>{
+            // event.target identifies which elemtent was clicked. 
+            // this logic determines that if the element is a child of the ref element, 
+            // we return early, avoiding passing a false bolean to setOpen, causing the dropdown to not work as expected
+            if (ref.current.contains(event.target)) {   
+                return;
+            }
+            setOpen(false);
+        });
+    }, []);
 
     const renderedItems = items.map((attribute)=>{
 
@@ -20,7 +33,7 @@ const Dropdown = ({items, selected, onSelectedChange}) => {
     });
 
     return(
-        <div className='ui form'>
+        <div ref={ref} className='ui form'>
             <div className='field'>
                 <label className='label'>Select A Title</label>
                 <div onClick={()=>setOpen(!open)} className={`ui selection dropdown ${open ? 'visible active' : ''}`}>
