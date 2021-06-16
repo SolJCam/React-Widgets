eda7ebf5afd033c3f7788557f6b2d4ff879aebd8
SolJCam <scameron110@gmail.com>
2021-06-15 23:58:04 -0400

    Successfully pinging google translation API and converting user input to desired language selection

--- src/components/Translate.js
+++ src/components/Translate.js
@@ -2,8 +2,6 @@ import React, {useState} from 'react';
 import Dropdown from './Dropdown.js'
 import Convert from './Convert.js'
 
-const googleTransApiKey = "AIzaSyCHUCmpR7cT_yDFHC98CZJy2LTms-IwDlM"
-
 const options = [
     {
         label: 'Afrikaans',
